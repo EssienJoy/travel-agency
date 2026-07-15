708ab95cf5c4eac7abba282264359ec6ac134157
essienjoy <ejoy7602@gmail.com>
2026-07-15 14:47:05 +0100

    Impliment Routing, Dashboard Layout and Ui, Auth Functionality

---
+++ app/routes/admin/all-users.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Header } from "../../../components";
+
+const AllUsers = () => {
+	return (
+		<main className='dasboard wrapper'>
+			<Header title={`Welcome Guest`} description='This is the users header' />
+
+			<p>All User Page Contents</p>
+		</main>
+	);
+};
+
+export default AllUsers;
